7ff6f6864104f37a4b59a6af13864161b8f9a060
Ahmed Raza <ahmedraza6305@gmail.com>
2024-11-03 16:46:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,33 +8,29 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
-// middleware
+// Connect to MongoDB once
+connectDB();
+
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: process.env.CLIENT_URL || "https://job-hunt-k9q7.vercel.app/",
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
-
-
-// api's
+// API routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+export default app;
