8ed60bf62c6f9adab4990e250471e353b4261af9
Ahmed Raza <ahmedraza6305@gmail.com>
2024-11-03 19:23:15 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
+import { login, logout, register, updateProfile, test } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { singleUpload } from "../middlewares/mutler.js";
  
@@ -7,11 +7,7 @@ const router = express.Router();
 
 router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
-router.route("/test").get((req, res)=>{
- res.json({
-  message:"hello"
-  )}
-});
+router.route("/test").get(test);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
 
